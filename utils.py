d56377a6bb041e4f1e50a922cb1bdc341ae06e89
milad <milad.4274@gmail.com>
2025-03-14 17:14:35 +0330

    add create gif function

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import random
+import imageio.v3 as iio
+import os
+
 
 def assign_edge_colors(graph, colors):
     """
@@ -19,3 +22,24 @@ def assign_edge_colors(graph, colors):
         edge_colors.append(color)
 
     return edge_colors
+
+
+def create_gif(path_gif, output_filename, duration=0.01):
+    """
+    Creates a looped GIF from PNG images in a folder.
+
+    Args:
+        path_gif: Path to the folder containing PNG images.
+        output_filename: Name of the output GIF file.
+        duration: Duration of each frame in seconds.
+    """
+    images = []
+    filenames = sorted([f for f in os.listdir(path_gif) if f.endswith('.png')])
+    for filename in filenames:
+        filepath = os.path.join(path_gif, filename)
+        images.append(iio.imread(filepath))
+
+    output_path = os.path.join(path_gif, output_filename)
+    iio.imwrite(output_path, images, fps=2, loop=0)
+
+
